2e29595e2ebce8e87bc5036ac2bf7e4cef1d9932
Faisal Khan <faisalkhan1222@gmail.com>
2016-06-06 12:51:43 -0400

    upto part2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function check(){
 
 pass = document.getElementById("pass").value;
@@ -23,3 +24,39 @@ function hasNumber(myString) {
     /\d/.test(
       myString));
 }
+*/
+/*part 1*/
+var person = function(fname, lname,age){
+  this.fname=fname,   this.lname=lname,   this.age=age;
+}
+
+objectPerson = new person("faisal","khan",20);
+
+for (property in objectPerson) {
+    console.log(property);
+}
+
+/*part 2*/
+
+var lastAnswer;
+
+function Multiplier(suppliedNumber){
+
+this.multiplyThis = function(){
+  timeOne = suppliedNumber * 1;
+  lastAnswer = suppliedNumber * timeOne;
+  console.log(lastAnswer);
+  return timeOne * suppliedNumber;
+}
+
+this.getCurrentValue = function(){
+  console.log(lastAnswer);
+  return lastAnswer;
+}
+}
+
+multiply = new Multiplier(3);
+multiply.multiplyThis();
+multiply.getCurrentValue();
+
+/*part 3*/
